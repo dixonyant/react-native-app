03840f18dc17cd4b652136024bbbfabda966ffb7
Dixon Yant <dixon@gatetothewest.com>
2025-06-07 22:03:40 -0700

    add small card shadow

--- GameOfSet/components/SetGameCard.tsx
+++ GameOfSet/components/SetGameCard.tsx
@@ -8,7 +8,7 @@ const CARD_HEIGHT = CARD_WIDTH * 1.6;
 const SetGameCard = ({ card }: { card: any })  => {
   console.log(card);
   return (
-    <View style={{ width: CARD_WIDTH, height: CARD_HEIGHT }} className="border border-gray-300 rounded-xl bg-white items-center justify-center mx-2">
+    <View style={{ width: CARD_WIDTH, height: CARD_HEIGHT }} className="border border-gray-300 rounded-xl bg-white items-center justify-center mx-2 shadow-sm">
       <View className="flex-1 flex-col items-center justify-center py-6" style={{ gap: CARD_HEIGHT * 0.06 }}>
         {[...Array(card?.number)].map((_, i) => (
           <Shape
